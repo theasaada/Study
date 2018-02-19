74496061341c473039db13fdc77020cadd34beac
Thea Saada <theasa@gmail.com>
2018-02-19 21:16:02 +0200

    change the comments

--- sum_of_odd_number.c
+++ sum_of_odd_number.c
@@ -7,7 +7,7 @@ int main()
 
     for (int i=0; i<5; i++)
     {
-        printf("Please add numbers of integer : \n" );      
+        printf("Please enter numbers of integer : \n" );      
         scanf("%d", &num);
         if ((num % 2) != 0)
             sum += num;
